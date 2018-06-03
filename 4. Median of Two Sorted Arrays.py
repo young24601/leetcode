75a247f6ae10e6fd04839c0fe1ca54546fcc2909
Young's Laptop <young24601@gmail.com>
2018-06-03 15:06:02 -0700

    laptop updates

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,3 +1,5 @@
+# INCOMPLETE
+
 # There are two sorted arrays nums1 and nums2 of size m and n respectively.
 # 
 # Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
@@ -14,26 +16,79 @@
 # The median is (2 + 3)/2 = 2.5
 
 
+"""
+Given two sorted arrays, pick the midpoint of the larger array.  In order for the solution
+to be O(log(n+m)), we must divide and ignore half of the parent array.
+
+Given two arrays, num1 and num2, if there is an odd number then the median will be the central
+value.  If there is an even number, the median will be the average of the two middle values.  
+
+"""
+
+
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: float
+        ==
+        A brute force solution would be to combine both arrays then find the median, which would be O(n+m) time.
+        
         """
-        # Combining the arrays would be O(m+n)
-        # A way to get O(log(m+n)) is to divide the array (arrays?) in half in each iteration
+        len1 = len(nums1)
+        len2 = len(nums2)
+        
+        if (len1 == 1) and (len2 == 1):
+            return (nums1[0] + nums2[0])/2
+        
+        print 'i:', len1/2, len2/2
+        #print nums1[len1/2], nums2[len2/2]
         
         
-        median = 0
-        
-        return median
 
+        return 'err'
 
-#####################
+###########################################3
+
 test = Solution()
 
+nums1 = [1]
+nums2 = [5]
+print 3, test.findMedianSortedArrays(nums1, nums2) #3
 
-nums1 = [1,3]
+nums1 = [1, 3]
 nums2 = [2]
-print("Answer:", test.findMedianSortedArrays(nums1,nums2))
+print 2, test.findMedianSortedArrays(nums1, nums2) #2
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+print 2.5, test.findMedianSortedArrays(nums1, nums2) #2.5
+
+nums1 = [1, 3]
+nums2 = [2, 4]
+print 2.5, test.findMedianSortedArrays(nums1, nums2) #2.5
+
+nums1 = []
+nums2 = [3, 4, 5]
+print 4, test.findMedianSortedArrays(nums1, nums2) #4
+
+nums1 = [3, 4]
+nums2 = []
+print 3.5, test.findMedianSortedArrays(nums1, nums2)
+
+nums1 = [1, 3, 5, 7]
+nums2 = [2, 4, 6]
+print 4, test.findMedianSortedArrays(nums1, nums2) #4
+
+nums1 = [2, 4, 6]
+nums2 = [1, 3, 5, 7]
+print 4, test.findMedianSortedArrays(nums1, nums2) #4
+
+nums1 = [1, 2, 3, 4]
+nums2 = [5, 6, 7]
+print 4, test.findMedianSortedArrays(nums1, nums2) #4
+
+nums1 = [5, 6, 7]
+nums2 = [1, 2, 3, 4]
+print 4, test.findMedianSortedArrays(nums1, nums2) #4
---
+++ 5. Longest Palindromic Substring.py
@@ -0,0 +1,91 @@
+"""
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+"""
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Naive way would be to go to each character and try to go left and right from it
+        # for as long as it remains a palindrome.
+
+        if len(s) <= 1:
+            return s
+
+        longestString = ""
+        
+        # There can be two types of palindromes fixing on a point n.  The letters could reflect on n
+        # like 123n321 (odd length) or could include n like 123nn321 (even length).
+        
+        for ct in range(0, len(s) - 1):
+            
+            #do odd length case first
+            left = ct - 1
+            right = ct + 1
+            currentString = s[ct]
+            while left > 0 and right < len(s):
+                if s[left] == s[right]:
+                    currentString = s[left] + currentString + s[right]
+                    if len(currentString) > len(longestString):
+                        longestString = currentString
+                    left -= 1
+                    right += 1
+                else:
+                    break
+                
+            #do even length case
+            if s[ct] == s[ct+1]:
+                #qualifies
+                left = ct - 1
+                right = ct + 2
+                currentString = s[ct] + s[ct+1]
+                while left > 0 and right < len(s):
+                    if s[left] == s[right]:
+                        currentString = s[left] + currentString + s[right]
+                        if len(currentString) > len(longestString):
+                            longestString = currentString
+                        left -= 1
+                        right += 1
+                    else:
+                        break
+        return longestString
+        
+        
+t = Solution()
+
+i = "cbbd" #output c, expected bb
+
+
+#i = "543qzcvdeqXqedvczq123" #odd
+#sol = t.longestPalindrome(i)
+#assert sol == "qzcvdeqXqedvczq" or sol == ""
+
+
+i = "abqxzzxqb" #even
+sol = t.longestPalindrome(i)
+assert sol == "bqxzzxqb" or sol == ""
+
+#i = "babad"
+#sol = t.longestPalindrome(i)
+#assert sol == "bab" or sol == "aba" or sol == ""
+
+#i = "cbbd"
+#sol = t.longestPalindrome(i)
+#assert sol == "bb" or sol == ""
+ 
